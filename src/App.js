0c1f59e3b7137a8163d7d0d930e2a3caf51a363f
K2adir <k2adir@gmail.com>
2023-03-22 15:55:34 -0400

    re-factored

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import UserCard from "./components/UserCard";
 function App() {
   return (
     <div className="App">
-      <h2>App</h2>
-
       <GetUsers />
       <UserCard />
     </div>
--- src/components/GetUsers.jsx
+++ src/components/GetUsers.jsx
@@ -1,74 +1,79 @@
 import React, { useState, useEffect } from "react";
 import "./GetUsers.css";
+
 const GetUsers = () => {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
 
-  /// fetch users
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
   const fetchData = async () => {
     const url = `https://randomuser.me/api/?page=${page}&results=10&seed=abc`;
 
     const res = await fetch(url);
     const users = await res.json();
     setUsers(users.results);
-    console.log(users.results);
   };
-  useEffect(() => {
-    fetchData();
-  }, [page]);
-  //// fetch user
-  //
-  //
+
+  const formatCountryName = (country) => {
+    let countryLowercase;
+    if (country === "United States") {
+      countryLowercase = "united-states-of-america";
+    } else if (country === "United Kingdom") {
+      countryLowercase = "united-kingdom";
+    } else if (country === "New Zealand") {
+      countryLowercase = "new-zealand";
+    } else {
+      countryLowercase = country.toLowerCase();
+    }
+    return countryLowercase;
+  };
+
+  const renderUser = ({
+    name: { first, last },
+    login: { uuid, username },
+    picture: { large },
+    location: { street, city, state, country, postcode },
+    email,
+  }) => {
+    const countryLowercase = formatCountryName(country);
+
+    return (
+      <div className="profile__card" key={uuid}>
+        <img className="profile__pic" src={large} alt="" />
+        <div>
+          <h1>
+            {first} {last}
+          </h1>
+          <h2>@{username}</h2>
+          <h3>{email}</h3>
+
+          <p>
+            {city}, {state}
+          </p>
+          <div className="profile__country">
+            <img
+              src={`https://cdn.countryflags.com/thumbs/${countryLowercase}/flag-400.png`}
+              className="flag"
+            />
+            <p>{country}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderUsers = () => {
+    return users.map(renderUser);
+  };
 
   return (
     <>
-      <div className="container">
-        {users.map(
-          ({
-            name: { first, last },
-            login: { uuid },
-            picture: { large },
-            location: { street, city, state, country, postcode },
-            email,
-            login: { username },
-          }) => {
-            let countryLowercase;
-            if (country === "United States") {
-              countryLowercase = "united-states-of-america";
-            } else if (country === "United Kingdom") {
-              countryLowercase = "united-kingdom";
-            } else {
-              countryLowercase = country.toLowerCase();
-            }
-
-            return (
-              <div className="profile__card" key={uuid}>
-                <img className="profile__pic" src={large} alt="" />
-                <div>
-                  <h1>
-                    {first} {last}
-                  </h1>
-                  <h2>@{username}</h2>
-                  <h3>{email}</h3>
-
-                  <p>
-                    {city}, {state}
-                  </p>
-                  <div className="profile__country">
-                    <img
-                      src={`https://cdn.countryflags.com/thumbs/${countryLowercase}/flag-400.png`}
-                      className="flag"
-                    />
-                    <p>{country}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-        )}
-      </div>
+      <div className="container">{renderUsers()}</div>
       <button
-        onClick={(e) => {
+        onClick={() => {
           const prevPage = page - 1;
           prevPage >= 1 ? setPage(prevPage) : setPage(setPage);
         }}
@@ -76,15 +81,8 @@ const GetUsers = () => {
       >
         Previous Page
       </button>
-      <button
-        onClick={(e) => {
-          const nextPage = page + 1;
-          setPage(nextPage);
-        }}
-      >
-        Next Page
-        {console.log(page)}
-      </button>
+
+      <button onClick={() => setPage(page + 1)}>Next Page</button>
     </>
   );
 };
