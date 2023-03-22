ad53a5370f4cdad4327586c3fc2baeeb5be27503
K2adir <k2adir@gmail.com>
2023-03-22 15:41:04 -0400

    pagination works

--- src/components/GetUsers.jsx
+++ src/components/GetUsers.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./GetUsers.css";
 const GetUsers = () => {
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
 
   /// fetch users
   const fetchData = async () => {
-    const url = `https://randomuser.me/api/?page=3&results=10&seed=abc
-`;
+    const url = `https://randomuser.me/api/?page=${page}&results=10&seed=abc`;
+
     const res = await fetch(url);
     const users = await res.json();
     setUsers(users.results);
@@ -14,40 +15,76 @@ const GetUsers = () => {
   };
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page]);
   //// fetch user
   //
   //
 
   return (
     <>
-      {users.map(
-        ({
-          name: { first, last },
-          login: { uuid },
-          picture: { large },
-          location: { street, city, state, country, postcode },
-        }) => {
-          return (
-            <div className="profile__card" key={uuid}>
-              <img src={large} alt="" />
-              <div>
-                <p>
-                  {first} {last}
-                </p>
+      <div className="container">
+        {users.map(
+          ({
+            name: { first, last },
+            login: { uuid },
+            picture: { large },
+            location: { street, city, state, country, postcode },
+            email,
+            login: { username },
+          }) => {
+            let countryLowercase;
+            if (country === "United States") {
+              countryLowercase = "united-states-of-america";
+            } else if (country === "United Kingdom") {
+              countryLowercase = "united-kingdom";
+            } else {
+              countryLowercase = country.toLowerCase();
+            }
 
-                <p>
-                  {street.number} {street.name}
-                </p>
-                <p>
-                  {city}, {state},{postcode}
-                </p>
-                <p> {country}</p>
+            return (
+              <div className="profile__card" key={uuid}>
+                <img className="profile__pic" src={large} alt="" />
+                <div>
+                  <h1>
+                    {first} {last}
+                  </h1>
+                  <h2>@{username}</h2>
+                  <h3>{email}</h3>
+
+                  <p>
+                    {city}, {state}
+                  </p>
+                  <div className="profile__country">
+                    <img
+                      src={`https://cdn.countryflags.com/thumbs/${countryLowercase}/flag-400.png`}
+                      className="flag"
+                    />
+                    <p>{country}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          );
-        }
-      )}
+            );
+          }
+        )}
+      </div>
+      <button
+        onClick={(e) => {
+          const prevPage = page - 1;
+          prevPage >= 1 ? setPage(prevPage) : setPage(setPage);
+        }}
+        disabled={page === 1}
+      >
+        Previous Page
+      </button>
+      <button
+        onClick={(e) => {
+          const nextPage = page + 1;
+          setPage(nextPage);
+        }}
+      >
+        Next Page
+        {console.log(page)}
+      </button>
     </>
   );
 };
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const UserCard = () => {
-  return <div>//</div>;
+  return <div></div>;
 };
 
 export default UserCard;
